adcbd9cb71056456c3a9163ca387e0637b942aed
Amrutha Iyyapu <amrutha.iyyapu0909@gmail.com>
2023-03-03 14:58:25 -0500

    Added forecast Temp changes and Search city
    Added default search for a city and also included functionality to update forecast temperature changes from celsius to fahrenheit.

--- src/index.js
+++ src/index.js
@@ -75,12 +75,12 @@ function displayForecast(response) {
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
+            <span class="weather-forecast-temperature-max forecast-temp">⬆️${Math.round(
               forecastDay.temp.max
-            )}° </span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(
+            )} </span><span>° </span>
+            <span class="weather-forecast-temperature-min forecast-temp"> ⬇️${Math.round(
               forecastDay.temp.min
-            )}° </span>
+            )}</span><span>° </span> 
           </div>
         </div>
     `;
@@ -99,57 +99,6 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-function updateCitynametemp(event) {
-  event.preventDefault();
-  let userCityInput = document.querySelector("#city-name");
-  if (userCityInput.value) {
-    let h3 = document.querySelector("h3");
-    cityName = userCityInput.value.toUpperCase();
-    h3.innerHTML = `${cityName}`;
-
-    let apiKey = "203fa770242fcd2b9555d832a88ea567";
-    function showTemp(response) {
-      // console.log(response.data);
-      let temperature = Math.round(response.data.main.temp);
-      // console.log(temperature);
-      let valueOfTemp = document.querySelector("#temp-value");
-      valueOfTemp.innerHTML = temperature;
-
-      let iconId = response.data.weather[0].icon;
-      let iconDesc = response.data.weather[0].description;
-
-      let icon = document.querySelector("#icon");
-      icon.setAttribute(
-        "src",
-        `https://openweathermap.org/img/wn/${iconId}@2x.png`
-      );
-      icon.setAttribute("alt", `${iconDesc}`);
-
-      let tempDescription = document.querySelector(
-        ".present-stats .city-timetemp-info .tempdescription"
-      );
-      tempDescription.innerHTML = response.data.weather[0].description;
-
-      let humid = document.querySelector(".Humidity");
-      humid.innerHTML = response.data.main.humidity;
-
-      let wind = document.querySelector(".Wind");
-      wind.innerHTML = Math.round(response.data.wind.speed);
-
-      let dateElement = document.querySelector(".current-day");
-      // console.log(response.data.dt);
-      let time = response.data.dt * 1000;
-      dateElement.innerHTML = addCurrentDay(time);
-
-      getForecast(response.data.coord);
-    }
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCityInput.value}&units=metric`;
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
-  } else {
-    prompt("please enter a valid city name");
-  }
-}
-
 function changeTempToF() {
   // event.preventDefault();
   let tempVariable = document.querySelector(".temp-type");
@@ -158,12 +107,18 @@ function changeTempToF() {
     let tempinF = valueOfTemp.innerHTML;
     tempinF = Number(tempinF);
     valueOfTemp.innerHTML = Math.round((tempinF * 9) / 5 + 32);
-
     let updateTempType = document.querySelector(".temp-type");
     updateTempType.innerHTML = "F";
   }
-}
 
+  let forecastTemp = document.querySelectorAll(".forecast-temp");
+  forecastTemp.forEach((elem) => {
+    let x = elem.innerHTML;
+    x = Math.round((x * 9) / 5 + 32);
+    // console.log(x);
+    elem.innerHTML = `${x}`;
+  });
+}
 function changeTempToC() {
   // event.preventDefault();
   let tempVariable = document.querySelector(".temp-type");
@@ -178,10 +133,15 @@ function changeTempToC() {
     // console.log(typeof updateTempType);
     updateTempType.innerHTML = "C";
   }
-}
 
-let form = document.querySelector("form");
-form.addEventListener("submit", updateCitynametemp);
+  let forecastTemp = document.querySelectorAll(".forecast-temp");
+  forecastTemp.forEach((elem) => {
+    let x = elem.innerHTML;
+    x = Math.round(((x - 32) * 5) / 9);
+    // console.log(x);
+    elem.innerHTML = `${x}`;
+  });
+}
 
 let tempCelsius = document.querySelector("#temp-c");
 tempCelsius.addEventListener("click", changeTempToC);
@@ -192,35 +152,35 @@ tempFar.addEventListener("click", changeTempToF);
 function showPresentTemp() {
   function updateTemp(response) {
     // console.log(response.data);
-    let newTemp = document.querySelector("#temp-value");
-    let currentTemp = Math.round(response.data.main.temp);
-    // console.log(currentTemp);
-    newTemp.innerHTML = `${currentTemp}`;
+    let tempC = document.querySelector(".temp-type");
 
+    if (tempC.innerHTML == "C") {
+      let newTemp = document.querySelector("#temp-value");
+      let currentTemp = Math.round(response.data.main.temp);
+      newTemp.innerHTML = `${currentTemp}`;
+    } else {
+      let newTemp = document.querySelector("#temp-value");
+      let currentTemp = Math.round((response.data.main.temp * 9) / 5 + 32);
+      newTemp.innerHTML = `${currentTemp}`;
+    }
     let newCity = document.querySelector("h3");
     newCity.innerHTML = response.data.name.toUpperCase();
-
     let tempDescription = document.querySelector(
       ".present-stats .city-timetemp-info .tempdescription"
     );
     tempDescription.innerHTML = response.data.weather[0].description;
-
     let iconId = response.data.weather[0].icon;
     let iconDesc = response.data.weather[0].description;
-
     let icon = document.querySelector("#icon");
     icon.setAttribute(
       "src",
       `https://openweathermap.org/img/wn/${iconId}@2x.png`
     );
     icon.setAttribute("alt", `${iconDesc}`);
-
     let wind = document.querySelector(".Wind");
     wind.innerHTML = Math.round(response.data.wind.speed);
-
     let humid = document.querySelector(".Humidity");
     humid.innerHTML = response.data.main.humidity;
-
     let dateElement = document.querySelector(".current-day");
     let time = response.data.dt * 1000;
     // debugger;
@@ -234,10 +194,8 @@ function showPresentTemp() {
     // console.log(lat);
     // console.log(lon);
     let apiKey = "203fa770242fcd2b9555d832a88ea567";
-
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(updateTemp);
-
     let apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl2).then(displayForecast);
   }
@@ -246,3 +204,50 @@ function showPresentTemp() {
 
 let button = document.querySelector("#current-temp-button");
 button.addEventListener("click", showPresentTemp);
+
+function updateCitynametemp(response) {
+  let h3 = document.querySelector("h3");
+  cityName = response.data.name.toUpperCase();
+  h3.innerHTML = `${cityName}`;
+  let temperature = Math.round(response.data.main.temp);
+  // console.log(temperature);
+  let valueOfTemp = document.querySelector("#temp-value");
+  valueOfTemp.innerHTML = temperature;
+  let iconId = response.data.weather[0].icon;
+  let iconDesc = response.data.weather[0].description;
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${iconId}@2x.png`
+  );
+  icon.setAttribute("alt", `${iconDesc}`);
+  let tempDescription = document.querySelector(
+    ".present-stats .city-timetemp-info .tempdescription"
+  );
+  tempDescription.innerHTML = response.data.weather[0].description;
+  let humid = document.querySelector(".Humidity");
+  humid.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector(".Wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector(".current-day");
+  let time = response.data.dt * 1000;
+  dateElement.innerHTML = addCurrentDay(time);
+  getForecast(response.data.coord);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let userCityInput = document.querySelector("#city-name");
+  search(userCityInput.value);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+function search(city) {
+  let apiKey = "203fa770242fcd2b9555d832a88ea567";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(updateCitynametemp);
+}
+
+search("Boston");
